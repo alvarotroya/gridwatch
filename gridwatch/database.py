7e2904ba7cc83698d55f449bca0d03e64915b217
Álvaro Troya <alvaro.troya.ec@googlemail.com>
2024-11-28 20:32:45 +0100

    Make db host configurable

--- gridwatch/database.py
+++ gridwatch/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from gridwatch.settings import app_settings
 
-DATABASE_URL = f"postgresql://{app_settings.db_user}:{app_settings.db_password}@localhost:{app_settings.db_port}/{app_settings.db_name}"
+DATABASE_URL = f"postgresql://{app_settings.db_user}:{app_settings.db_password}@{app_settings.db_host}:{app_settings.db_port}/{app_settings.db_name}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- gridwatch/settings.py
+++ gridwatch/settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    db_host: str
     db_user: str
     db_password: str
     db_name: str
